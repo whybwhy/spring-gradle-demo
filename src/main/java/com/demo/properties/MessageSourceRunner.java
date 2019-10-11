c5beab85f6fb2f293ac2fcea7a2433d286ac284d
whybwhy <whybwhy@gmail.com>
2019-10-11 10:28:25 +0900

    properties - 다국어 처리

---
+++ src/main/java/com/demo/properties/MessageSourceRunner.java
@@ -0,0 +1,24 @@
+package com.demo.properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class MessageSourceRunner implements ApplicationRunner {
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(messageSource.getMessage("message.language", new String[]{"안녕하세요"}, Locale.KOREA ));
+        System.out.println(messageSource.getMessage("message.language2", new String[]{"안녕하세요"}, "Not Found", Locale.KOREA ));
+        System.out.println(messageSource.getMessage("message.language", new String[]{"hi"}, Locale.ENGLISH));
+
+    }
+}
